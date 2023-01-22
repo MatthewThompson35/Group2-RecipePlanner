9a9b3098a29a463257f97929af1d10970ded8800
DHarper04 <dharpe11@my.westga.edu>
2023-01-22 18:10:01 -0500

    Added sample test code
    Created NUnit test project and tied it to the web application project functionality. Created model class with simple math functionality to integrate unit testing and ensure it works within the project

---
+++ code/web/RecipePlannerWebApplication/PrototypeWebApplication/Models/Prototype.cs
@@ -0,0 +1,12 @@
+﻿namespace PrototypeWebApplication.Models
+{
+    public class Prototype
+    {
+
+        public int sum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+    }
+}
