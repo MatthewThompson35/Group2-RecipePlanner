e9490903c788090a8cc033c32ba97995fcab5fd0
Matthew Thompson <mthomp35@my.westga.edu>
2023-02-02 11:27:32 -0500

    Created database connection for the web side

---
+++ code/web/RecipePlannerWebApplication/PrototypeWebApplication/Models/NameDAL.cs
@@ -0,0 +1,25 @@
+﻿namespace PrototypeWebApplication.Models
+{
+    using MySql.Data.MySqlClient;
+
+    public class NameDAL
+    {
+
+        public List<String> getAllNames()
+        {
+            using var connection = new MySqlConnection(Connection.ConnectionString);
+            connection.Open();
+            var query = "Select * from names;";
+            using var command = new MySqlCommand(query, connection);
+            using var reader = command.ExecuteReader();
+            var names = new List<String>();
+            while (reader.Read())
+            {
+                names.Add(reader.GetString(0));
+
+            }
+
+            return names;
+        }
+    }
+}
